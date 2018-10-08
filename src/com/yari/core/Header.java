c3eaef8949c5ea19e8dbcd35e2148dea954033cc
root <ahmed.botan@gmail.com>
2018-10-08 19:25:25 +0200

    Print OS INFORMATION

--- src/com/yari/core/Header.java
+++ src/com/yari/core/Header.java
@@ -11,7 +11,7 @@ class Header {
 
     private volatile static short MAJOR_VERSION = 0;
     private volatile static short VERSION = 2;
-    private static short MINOR_VERSION = 8;
+    private static short MINOR_VERSION = 12;
 
 
     static void show() throws Exception {
@@ -32,7 +32,28 @@ class Header {
 
         System.out.println("Version : " + (MAJOR_VERSION + "." + VERSION + "." + MINOR_VERSION));
 
+        System.out.println();
 
+        System.out.println("OS name : " + System.getProperty("os.name"));
+        System.out.println("OS architecture : " + System.getProperty("os.arch"));
+        System.out.println("OS version : " + System.getProperty("os.version"));
+
+        System.out.println();
+
+        System.out.println("Java version : " + System.getProperty("java.version"));
+        System.out.println("Java vendor : " + System.getProperty("java.vendor"));
+        System.out.println("Java home : " + System.getProperty("java.home"));
+        System.out.println("Java Virtual Machine version : " + System.getProperty("java.vm.version"));
+
+        System.out.println();
+
+        System.out.println("Available processors (cores) : " + Runtime.getRuntime().availableProcessors());
+        System.out.println("Free memory : " + Runtime.getRuntime().freeMemory() / 1000 + " MB");
+
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        System.out.println("Maximum memory : " + (maxMemory == Long.MAX_VALUE ? "unlimited" : maxMemory / 1000 + " MB"));
+
+        System.out.println("Total memory available to JVM : " + Runtime.getRuntime().totalMemory() / 1000 + " MB\n");
     }
 
     private static void updateVersion() throws Exception {
--- src/com/yari/network/game/GameServer.java
+++ src/com/yari/network/game/GameServer.java
@@ -1,14 +1,19 @@
 package com.yari.network.game;
 
 import com.yari.api.Service;
+import com.yari.injector.api.InjectConfiguration;
 import com.yari.network.api.NetworkService;
 
 
 @NetworkService
 public class GameServer implements Service {
 
+    @InjectConfiguration("network-port")
+    private int port;
+
     @Override
     public void start() {
+
     }
 
     @Override
